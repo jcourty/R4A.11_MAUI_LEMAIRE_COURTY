f8f1e49803aa215d7c08bb2dd75b726e72bb7ecf
LESCHAEVE--LEMAIRE Nathaniel <nathaniel.leschaevelemaire@etu.unilim.fr>
2026-03-04 18:33:25 +0100

    Création du projet

---
+++ NathanielJulieApp/NathanielJulieApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NathanielJulieApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NathanielJulieApp/NathanielJulieApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NathanielJulieApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
